163a56f8fd158fcba7899040a47a5e9de9d10ff2
Aniket Dahotre <dahotre.aniket@gmail.com>
2013-06-11 11:21:39 -0600

    Initial scaffold works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require handlebars
 //= require_tree .
+
+$(function() {
+
+    $('footer a').click(function (event) {
+        event.preventDefault();
+        console.log('clicked');
+        var next_page_link = $(this).attr('href');
+        console.log('href: ' + next_page_link);
+        var next_page_number = parseInt(next_page_link.split('=')[1]) + 1;
+        var base_url = next_page_link.split('=')[0] + '=';
+        $(this).attr('href', base_url + next_page_number);
+        $.getJSON(next_page_link, function(quotes) {
+            var new_quotes = '';
+            $.each(quotes, function(key, val) {
+                new_quotes = new_quotes + "<tr class='quote'><td>" + val.author + "</td><td>" + val.quote + "</td></tr>";
+            });
+
+            $('#quotesTable').append(new_quotes);
+        });
+    });
+
+});
