21260f9060b4dbb82ddde94c3c53f0c184817153
Aniket Dahotre <dahotre.aniket@gmail.com>
2013-06-10 19:01:33 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5Storage::Application.config.secret_token = '967f461d02f4410ba0353e3b8d5f63e0b54148d22932f102f04beb30f55e404f1040628903b65bd575beb75d372cab57d8a6be6b403e95afb90db71c2fa6291f'
